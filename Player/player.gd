d141ca73bcf357ff3e58997df89d4d7a5b6fc29a
Chaeeun147 <chaeeunlim.love@gmail.com>
2025-11-02 17:17:33 -0500

    Moving the Player

---
+++ Player/player.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var speed: float = 150
+var vel := Vector2 (0,0)
+
+func _physics_process(delta):
+	var dirVec := Vector2(0,0)
+	if Input.is_action_pressed("move_left"):
+		dirVec.x = -1
+	elif Input. is_action_pressed("move_right"):
+		dirVec.x = 1
+	if Input.is_action_pressed("move_up"):
+		dirVec.y = -1
+	elif Input. is_action_pressed("move_down"):
+		dirVec.y = 1
+
+	vel = dirVec.normalized () * speed
+	position += vel * delta
