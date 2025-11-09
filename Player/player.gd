d41d2fa019d0490af3f3d05d6a982f6889607fb9
Chaeeun147 <chaeeunlim.love@gmail.com>
2025-11-08 23:18:54 -0500

    Animating the Player and Making the Buller
    - Animated the player to look like it was going right when going right, and left when it was going left
    - Made bullets shoot out the of the wings of player

--- Player/player.gd
+++ Player/player.gd
@@ -1,22 +1,43 @@
 extends Area2D
 
-var speed: float = 150
-var vel := Vector2 (0,0)
+var plBullet := preload("res://Bullet/bullet.tscn")
 
+@onready var sprite := $Sprite2D
+@onready var firingPositions := $FiringPositions
+
+@export var speed: float = 100
+var vel := Vector2(0,0)
+
+func _process(delta):
+	if vel.x <0:
+		sprite.frame = 0
+	elif vel.x > 0:
+		sprite.frame = 2
+	else:
+		sprite.frame = 1
+		
+# check if we are shooting
+	if Input.is_action_pressed("shoot"):
+		for child in firingPositions.get_children():
+			var bullet := plBullet.instantiate()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
+	
 func _physics_process(delta):
 	var dirVec := Vector2(0,0)
 	if Input.is_action_pressed("move_left"):
 		dirVec.x = -1
-	elif Input. is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		dirVec.x = 1
 	if Input.is_action_pressed("move_up"):
 		dirVec.y = -1
-	elif Input. is_action_pressed("move_down"):
+	elif Input.is_action_pressed("move_down"):
 		dirVec.y = 1
 
-	vel = dirVec.normalized () * speed
+	vel = dirVec.normalized() * speed
 	position += vel * delta
-	#make sure we are within the screen
-	var viewRect = get_viewport().get_visible_rect()
+	
+	# make sure that we are within the screen
+	var viewRect := get_viewport_rect()
 	position.x = clamp(position.x, 0, viewRect.size.x)
 	position.y = clamp(position.y, 0, viewRect.size.y)
