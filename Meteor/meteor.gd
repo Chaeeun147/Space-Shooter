23cadb1dbde581616f63072118110be115e43684
Chaeeun147 <chaeeunlim.love@gmail.com>
2025-11-15 21:22:46 -0500

    Meteors
    Adding Meteors that side down the screen

---
+++ Meteor/meteor.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@export var minSpeed: float = 10
+@export var maxSpeed: float = 20
+@export var minRotationRate: float = -20
+@export var maxRotationRate: float = 20
+
+var speed : float = 0
+var rotationRate: float = 0
+
+func _ready():
+	speed = randf_range(minSpeed, maxSpeed)
+	rotationRate = randf_range(minRotationRate, maxRotationRate)
+
+func _physics_process(delta):
+	rotation_degrees += rotationRate * delta
+	
+	position.y += speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+	pass # Replace with function body.
